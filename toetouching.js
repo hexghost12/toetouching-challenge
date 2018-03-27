94524eef2624999535dbf2b08b62bc54f8390210
Sergey Serebryakov <hellomind@gmail.com>
2018-03-27 14:10:13 -0700

    backfill

--- toetouching.js
+++ toetouching.js
@@ -1,8 +1,16 @@
 ([
   {
+    date: 'Mar 27, 2018',
+    andrey:  0,
+    sergey:  0,
+    timur:   0,
+    ira:     0,
+    sergeyv: 0
+  },
+  {
     date: 'Mar 26, 2018',
     andrey:  1,
-    sergey:  0,
+    sergey:  1,
     timur:   0,
     ira:     0,
     sergeyv: 0
@@ -10,7 +18,7 @@
   {
     date: 'Mar 25, 2018',
     andrey:  1,
-    sergey:  0,
+    sergey:  1,
     timur:   0,
     ira:     1,
     sergeyv: 1
