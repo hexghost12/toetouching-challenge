076008962b7e2a8965a680db2cc3b742348a85f1
Ira <ivasilinets@users.noreply.github.com>
2018-03-27 22:00:51 -0700

    Update toetouching.js

--- toetouching.js
+++ toetouching.js
@@ -4,16 +4,16 @@
     andrey:  0,
     sergey:  0,
     timur:   0,
-    ira:     0,
-    sergeyv: 0
+    ira:     1,
+    sergeyv: 1
   },
   {
     date: 'Mar 26, 2018',
     andrey:  1,
     sergey:  1,
     timur:   0,
-    ira:     0,
-    sergeyv: 0
+    ira:     1,
+    sergeyv: 1
   },
   {
     date: 'Mar 25, 2018',
